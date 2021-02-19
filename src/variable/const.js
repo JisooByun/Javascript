971a1077cdd8593d07ead8c5fbc48025eeefe35d
byun <bjs1616@gmail.com>
2021-02-19 09:53:00 +0900

    변수

---
+++ src/variable/const.js
@@ -0,0 +1,21 @@
+const calculator = {
+    sum: (a,b)=>{
+        return a+b;
+    },
+    a:100
+}
+calculator.minus = (a,b) =>{
+    return a-b;
+}
+calculator.sum = (a,b) =>{
+    return a*b;
+}
+calculator.a = 200;
+console.log(calculator.minus(10,20))
+console.log(calculator.sum(10,20))
+console.log(calculator.a)
+
+calculator = {
+
+}
+
